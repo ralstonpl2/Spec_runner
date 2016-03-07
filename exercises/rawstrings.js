4985e59881028399106dc012d0e8ef818759e306
Ralston Louie <ralstonlouie@gmail.com>
2016-03-07 00:05:03 -0800

    Passing third test

--- exercises/rawstrings.js
+++ exercises/rawstrings.js
@@ -12,7 +12,8 @@ Feel free to use any string methods you wish.
 // Gives back ("returns") the fourth letter in a string
 var printLetter1 = function () { 
 	console.log("This is Working!");
-  	return'print the fourth letter in this string'[3];
+	console.log("print the fourth letter in this string"[3]);
+  	return"print the fourth letter in this string"[3];
 
 
  
